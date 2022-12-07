05dd2ddd894fd53065b0077a54b6e474c64cf77c
marius321967 <marius.dras@gmail.com>
2022-12-07 17:36:56 +0200

    Topic #2 exercises

---
+++ lesson2/exercise.php
@@ -0,0 +1,72 @@
+<?php
+declare(strict_types=1);
+
+/*
+1. Pašalinkite/užkomentuokite praeitos užduoties eilutes, kad jos netrukdytų toliau tęsti darbo.
+Išveskite į terminalą kintamojo $variableToDebug:
+- tik reikšmę
+- reikšmę kartu su duomens tipu
+*/
+$variableToDebug = 15;
+
+/*
+2. Sudėliokite masyvą ir jį atspausdinkite iš šių reikšmių:
+- Hello
+- Learning
+- Amazing
+- CodeAcademy
+- Php
+- Git
+*/
+
+/*
+3. Pakeiskite duotų kintamųjų reikšmes į:
+- int
+- bool
+*/
+
+//pakeisti i int:
+$variable1 = false;
+$variable2 = true;
+$variable3 = '2022 year';
+
+//pakeisti i bool:
+$variable4 = 150;
+$variable5 = [];
+$variable6 = 'Year - 2022';
+
+/*
+4. Atlikite šiuo loginius operatorius su duotais kintamaisiais taip, kad gautumėt true ir false
+- &&
+- ||
+*/
+
+$variable7 = 145;
+$variable8 = '145';
+$variable9 = '150';
+$variable10 = 150;
+
+/*
+5. Atlikite siuos skaiciavimus, panaudodami kintamuosius
+- 143 * 444
+- 567 / 12
+- 578 % 55
+- 6 ** 5
+- 5--;
+- 6++;
+*/
+
+/*
+6. Parasykite koda, kad jis patikrintu ar:
+- 56 > 77
+- 190 <= 230
+- 230 yra lygu '230'
+- 45 nera lygu '45
+*/
+
+/*
+7. Parasykite koda, kad jis ar reiksme egzistuoja
+- pasirasykite kintamaji $variable = 10; patikrinkite ar jis egzistuoja;
+- jeigu egzistuoja patikrinkite koks jo tipas
+- istrinkite jo apsirasyma ir patikrinkite ar egzistuojate. kokia reiksme gaunate? ar gaunate error?
+*/
