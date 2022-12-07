a8456bbc78775b62af28c6ec6f8bf4b47a2d5906
marius321967 <marius.dras@gmail.com>
2022-12-07 21:03:11 +0200

    More exercises

---
+++ exercise2.php
@@ -0,0 +1,9 @@
+<?php
+
+// 1. Sukurkite konstantą WEBSITE_ADDRESS, reikšmėje nurodykite google.com adresą
+
+// 2. Palyginkite reikšmes 200 ir '200' taip, kad:
+//   a) gautume true
+//   b) gautume false
+
+// 3. Patikrinkite ar galime sujungti du skaičius kaip string duomenis (string sujungimui naudojame taško simbolį)
