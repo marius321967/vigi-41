86da1c1a058c5fb29c1792ffe5c8b257fc101fd0
marius321967 <marius.dras@gmail.com>
2022-12-08 17:38:40 +0200

    PHP intro #3 exercises

---
+++ lesson3/exercise.php
@@ -0,0 +1,92 @@
+<?php
+declare(strict_types=1);
+
+/*
+1. Apskaičiuokite PHP pagalba ir išveskite į terminalą. Kiekvienas rezultatas turi būti naujoje eilutėje:
+
+987 + 545 - 32 * 94
+32 pakelkite laipsniu 3 ir pridėkite 18
+120 padalinkite iš 4 ir dar karta padalinkite iš 3
+kokia liekana lieka po skaičiaus 187 dalybos iš 5
+skaičiui 3 tris kartus pritaikykite increment operatorių - koki skaičių gaunate?
+skaičiui 12 keturis kartus pritaikykite decrement operatorių - koki skaičių gaunate?
+*/
+
+/*
+2. Išspausdinkite skaičius nuo 1 iki 10 naudodamiesi ciklu. Panaudokite visus 4 būdus ciklams aprašyti.
+Kiekvienas skaičius turi išspausdintas naujoje eilutėje.
+
+1
+2
+3
+...
+*/
+
+/*
+3. Išspausdinkite skaičius nuo 15 iki 3 naudodamiesi ciklu. Panaudokite sau patogiausią ciklą.
+Kiekvienas skaičius turi išspausdintas naujoje eilutėje.
+*/
+
+/*
+4. Išspausdinkite kas antrą skaičių nuo 1 iki 20 naudodamiesi ciklu.
+Kiekvienas skaičius turi išspausdintas naujoje eilutėje.
+
+1
+3
+5
+...
+*/
+
+/*
+5. Išspausdinkite skaičius, nuo 1 iki 20, kurie dalijasi iš 3.
+Kiekvienas skaičius turi išspausdintas naujoje eilutėje.
+*/
+
+/*
+6. Išspausdinkite skaičius, nuo 1 iki 20, kurie dalijasi iš 3 arba iš 5.
+Kiekvienas skaičius turi išspausdintas naujoje eilutėje.
+*/
+
+/*
+7. Iteruokite per skaičius, nuo 1 iki 20.
+Jeigu skaičius dalijasi iš 3, išspausdinkite žodį 'Hey'.
+Jeigu skaičius dalijasi iš 5, išspausdinkite žodį Ho'.
+Jeigu skaičius dalijasi ir iš 5, ir iš 3, išspausdinkite žodį 'HeyHo'.
+Kitu atveju išspausdinkite skaičių.
+Viskas turi būti atspausdinti vienoje eilutėje su tarpais:
+1 2 Hey 4 Ho Hey 7 8 Hey Ho 11 Hey 13 14 HeyHo 16 17 Hey 19 Ho
+*/
+
+/*
+8. Raskite sveikų skaičių nuo 1 iki 100 sumą.
+*/
+
+/*
+9. Pasinaudodami ciklu išspausdinkite savaitės dienas iš masyvo $days vienoje eilutėje:
+monday-tuesday-wednesday-thursday-friday-saturday-sunday-
+*/
+
+$days = [
+    'monday',
+    'tuesday',
+    'wednesday',
+    'thursday',
+    'friday',
+    'saturday',
+    'sunday',
+];
+
+/*
+10. Iteruokite sveikus skaičius nuo -5 iki 5.
+Išspausdinkite skaičių dvejopai:
+1. Pasinaudojant paprastu echo
+2. Pasinaudojant funkcija var_dump ir prieš tai pavertus į 'bool' tipo reikšmę
+
+-5
+bool(true)
+-4
+bool(true)
+...
+
+HINT: atkreipkite dėmesį į ką pavirsta skaičius 0
+*/
