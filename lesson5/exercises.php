7bbbe6409e0d0843363d4e2af68c12f49c57a6d4
marius321967 <marius.dras@gmail.com>
2022-12-13 17:49:01 +0200

    #5 exercises copied

---
+++ lesson5/exercises.php
@@ -0,0 +1,58 @@
+<?php
+
+// Visur sudėkite reikiamus parametrų bei return tipus
+
+/*
+1. Parašykite funkciją 'dividesBy5', kuri priimtų int tipo skaičių ir grąžintų jo dalybos iš 5 liekaną.
+*/
+
+/*
+2. Parašykite funkciją 'arrayPrinter', kuri priimtų array tipo parametrą ir
+išspausdintų kiekvieną masyvo elementą naujoje eilutėje.
+
+Funkcijos kvietimas: arrayPrinter(['some text', 'another text'])
+Funkcija grąžina: funkcija nieko negrąžina - ji tik išspausdina masyvą į terminalą:
+'some text'
+'another text'
+...
+*/
+
+/*
+3. Parašykite funkciją 'stringEnhancer', kuri grąžintų pakeistą tekstą.
+
+Funkcijos kvietimas: stringEnhancer('some text', '##')
+Funkcija grąžina: '##some text##'
+
+Funkcijos kvietimas: stringEnhancer('some text')
+Funkcija grąžina: '**some text**'
+*/
+
+/*
+4. Parašykite funkciją 'stringModifier', kuri pamodifikuotų paduotą string tipo kintamąjį.
+
+Funkcijos kvietimas:
+$x = 'some text';
+stringModifier($x, '##');
+echo $x; // '##some text##'
+
+Funkcija grąžina: funkcija nieko negrąžina
+
+*/
+
+/*
+4. Parašykite funkciją 'textReplicator', kuri grąžintų 'padaugintą' tekstą.
+
+Funkcijos kvietimas:
+textReplicator('some_text', 3);
+
+Funkcija grąžina: 'some_text-some_text-some_text'
+
+Funkcijos kvietimas:
+textReplicator('some_text', null);
+
+Funkcija grąžina: 'some_text'
+*/
+
+/*
+4. Paverskite funkciją 'textReplicator', į veikiančią anoniminę funkciją.
+*/
---
+++ lesson5/file.php
@@ -0,0 +1,10 @@
+<?php
+
+$email = '';
+
+var_dump(kahoot('butkute@gmail.com'));
+
+function kahoot(string $email): string {
+    return $email = 'diana@gmail.com';
+}
+
