7e077e3952f4e42237f45382291335c4c95ff15a
marius321967 <marius.dras@gmail.com>
2023-02-05 19:37:38 +0200

    lesson 8 composer exercises

---
+++ oop/lesson8/composer_excercises.php
@@ -0,0 +1,20 @@
+1.1
+Inicializuokite nauja composer projekta l6/1_inventory_checker.php uzduoties kataloge.
+Projekto pavadinimas 'jusu-vardas-pavarde/inventory-checker'
+
+1.2
+Nustatykite susiekite savo projekto namespace su src/ direktorija
+
+1.3
+Pakeiskite savo rašytą autoloaderį Composer'io autoloaderiu
+
+1.4 Įsirašykite Monolog loggerį (https://packagist.org/packages/monolog/monolog) ir naudokite jį log'inimui vietoje savo
+pačių rašyto kodo
+
+1.5
+Įsidiekite 'php-cs-fixer' įrankį į savo projekto dev dependencies. Paleiskite įrankį ir sutvarkykite klaidas, kurias jis rado
+
+https://github.com/FriendsOfPHP/PHP-CS-Fixer
+
+1.6 Ištrinkite vendor/ direktorija ir vėl iš naujo suinstaliuokite visus dependencius
+
